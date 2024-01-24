ab718a5ba47a55909b9623539acdda499adae4c6
MikaChueWaye <mika.chuewaye@gmail.com>
2024-01-24 06:16:59 +0100

    ajout des animations de l'esprit

--- assets/scripts/Spirit.gd
+++ assets/scripts/Spirit.gd
@@ -16,13 +16,26 @@ func get_input():
 	if Input.is_action_pressed('ui_up'):
 		target_velocity.y -= 1
 	target_velocity = target_velocity.normalized() * speed
-	
-	if target_velocity == Vector2.ZERO:
-		velocity = velocity.linear_interpolate(Vector2.ZERO, deceleration)
-	else:
-		velocity = velocity.linear_interpolate(target_velocity, acceleration)
+	spirit_movement_effect(target_velocity)
 
 func _physics_process(delta):
 	if get_parent().meditating:
 		get_input()
 		move_and_collide(velocity * delta)
+		
+func launch_spirit(body_direction):
+	var direction = 2
+	if body_direction:
+		direction = -2
+	
+	var target_velocity = Vector2(direction, -1)
+	target_velocity = target_velocity.normalized() * 1600
+	spirit_movement_effect(target_velocity)
+		
+func spirit_movement_effect(target_velocity):
+	if target_velocity == Vector2.ZERO:
+		velocity = velocity.linear_interpolate(Vector2.ZERO, deceleration)
+	else:
+		velocity = velocity.linear_interpolate(target_velocity, acceleration)
+	
+	
